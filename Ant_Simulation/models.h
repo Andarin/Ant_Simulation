9b6c2973889fe9d385769508ef8a837e5ad771cd
Andarin <lucas.tittmann@ensae.fr>
2013-01-10 01:26:13 +0100

    Second start after some lousy errors...version 0.4 - 100 000 ants

---
+++ Ant_Simulation/models.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "windows.h"
+#include <SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <iostream>
+
+#ifndef MODELS_H
+#define MODELS_H
+
+void draw_board(int,unsigned int);
+void draw_border(int,unsigned int);
+void draw_ant(int);
+
+#endif
