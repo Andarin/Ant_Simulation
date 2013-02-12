604391e0c3cdd1f573ec0c9692c6d1d0e459564a
GGMC <guillaume.martinet@ensae.fr>
2013-02-12 16:24:47 +0100

    Pheromone Matrix created in Tables_of_objects

--- Ant_Simulation/Table_of_objects.cpp
+++ Ant_Simulation/Table_of_objects.cpp
@@ -1,8 +1,11 @@
 #include "Table_of_objects.h"
+#include <algorithm>	
 
-
-Table_of_objects::Table_of_objects(void)
+Table_of_objects::Table_of_objects(int n)
 {
+	std::vector<std::shared_ptr<Hormone>> row_vect ;
+	row_vect.resize(n);
+	_pheromone_matrix.assign(n,row_vect);
 }
 
 
@@ -11,15 +14,23 @@ Table_of_objects::~Table_of_objects(void)
 }
 
 void Table_of_objects::add_ant (std::shared_ptr<Ant> p_ant){
+	if (std::find(_ant_list.begin(), _ant_list.end(), p_ant) == _ant_list.end())
 	_ant_list.push_back (p_ant);
 }
 
 void Table_of_objects::add_colony (std::shared_ptr<Colony> p_colony){
+	if (std::find(_colony_list.begin(), _colony_list.end(), p_colony) == _colony_list.end())
 	_colony_list.push_back (p_colony);
 }
 
 void Table_of_objects::add_food (std::shared_ptr<Food> p_food){
+	if (std::find(_food_list.begin(), _food_list.end(), p_food) == _food_list.end())
 	_food_list.push_back (p_food);
+}
+
+void Table_of_objects::add_pheromone (std::shared_ptr<Hormone> p_pheromone){
+	if (std::find(_pheromone_list.begin(), _pheromone_list.end(), p_pheromone) == _pheromone_list.end())
+	_pheromone_list.push_back (p_pheromone);
 }
 
 std::list<std::shared_ptr<Ant>> Table_of_objects::get_ant_list(){
@@ -32,4 +43,12 @@ std::list<std::shared_ptr<Colony>> Table_of_objects::get_colony_list(){
 
 std::list<std::shared_ptr<Food>> Table_of_objects::get_food_list(){
 	return _food_list ;
+}
+
+std::list<std::shared_ptr<Hormone>> Table_of_objects::get_pheromone_list(){
+	return _pheromone_list ;
+}
+
+std::vector<std::vector<std::shared_ptr<Hormone>>> Table_of_objects::get_pheromone_matrix(){
+	return _pheromone_matrix ;
 }
--- Ant_Simulation/Table_of_objects.h
+++ Ant_Simulation/Table_of_objects.h
@@ -3,22 +3,37 @@
 #include "Colony.h"
 #include "Food.h"
 #include <list>
+#include <vector>
 
-class Table_of_objects
-{
-public:
-	Table_of_objects(void);
-	~Table_of_objects(void);
+class Table_of_objects					// This table regroup all the objects of the game
+{								// It is created at the beginning and updated during the game
+public:									// We use smart pointers to identify the objects
+	Table_of_objects(int);		//The int input is the number of sudivisions of the matrix M
+	~Table_of_objects(void);			// representing the map, i.e. dim M = n*n
+	//Add functions :
 	void add_ant (std::shared_ptr<Ant>);
 	void add_colony (std::shared_ptr<Colony>);
 	void add_food (std::shared_ptr<Food>);
+	void add_pheromone (std::shared_ptr<Hormone>);
+	//Delete functions :
+	void delete_ant (std::shared_ptr<Ant>);
+	void delete_colony (std::shared_ptr<Colony>);
+	void delete_food (std::shared_ptr<Food>);
+	void delete_pheromone (std::shared_ptr<Hormone>);
+	//Get functions :
 	std::list<std::shared_ptr<Ant>> get_ant_list();
 	std::list<std::shared_ptr<Colony>> get_colony_list();
 	std::list<std::shared_ptr<Food>> get_food_list();
+	std::list<std::shared_ptr<Hormone>> get_pheromone_list();
+	std::vector<std::vector<std::shared_ptr<Hormone>>> get_pheromone_matrix();
 
 private:
+	//Lists :
 	std::list<std::shared_ptr<Ant>> _ant_list;
 	std::list<std::shared_ptr<Colony>> _colony_list;
 	std::list<std::shared_ptr<Food>> _food_list;
+	std::list<std::shared_ptr<Hormone>> _pheromone_list;
+	//Pheromone position matrix :
+	std::vector<std::vector<std::shared_ptr<Hormone>>> _pheromone_matrix ;
 };
 
