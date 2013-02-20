5ecf13841307d7ef0cbd1f871d41bc29752bf96e
Andarin <lucas.tittmann@ensae.fr>
2013-02-20 18:10:04 +0100

    Added parameter text file read.

--- Ant_Simulation/Ant_Sim.cpp
+++ Ant_Simulation/Ant_Sim.cpp
@@ -166,9 +166,9 @@ void Ant_Sim::add_colony(void)
 	colony_birth_info._colony_max_reproduction_speed = 200;
 	colony_birth_info._initial_food = 1000;
 	colony_birth_info._size = 100;
+	colony_birth_info.read_from_file("Ant_Sim_parameters.txt");
 	auto new_colony_ptr = std::make_shared<Colony>(colony_birth_info);
 	(*_table_items).add_colony(new_colony_ptr);
-	colony_birth_info.read_from_file("Ant_Sim_parameters.txt");
 }
 
 void Ant_Sim::add_food(double amount, int x, int y, int z)
